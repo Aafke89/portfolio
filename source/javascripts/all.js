b6bfe6e17c1d51131bcaeea9912b4cf095f794af
Aafke Smal <aafke.smal@gmail.com>
2016-05-26 19:19:03 +0200

    Opgeruimd staat netjes

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,6 +3,12 @@
 //= require_tree .
 
 
+// 1. Functions to show, close and drag boxes /////////
+// 2. Projects info /////////////
+// 3. Shuffle functions ////////////////
+
+////////////  1. Functions to show, close and drag boxes /////////////////
+
 function closeBoxes(){
   // Hide boxes
   $(".cross-box").on("click", function(){
@@ -17,44 +23,27 @@ function closeBoxes(){
   });
 };
 
-
-
-
-
-
-$(document).ready(function(){
-  closeBoxes();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // Make active if clicked on box
-  $(".box").on("click", function(){
-    $(".box").removeClass("active");
-    $(this).addClass("active");
-  })
+function infoBox(){
   // Show box on click in list
   $(".info-list").on("click", function(){
     var target =  $(this).data("target");
     $(target).removeClass("hidden");
     $(".box").removeClass("active");
     $(target).addClass("active");
-  })
+  });
+};
 
-  // Make boxes draggable\
+function activeBox(){
+  // Make active if clicked on box
+  $(".box").on("click", function(){
+    $(".box").removeClass("active");
+    $(this).addClass("active");
+  })
+}
+
+function boxDraggable(){
+  // Make boxes draggable
   var $dragging = null;
-  // var $position = null;
   $(document.body).on("mousemove", function(e) {
     if ($dragging) {
       $dragging.offset({
@@ -65,70 +54,89 @@ $(document).ready(function(){
   });
   $(".box").on("mousedown", function (e) {
     $dragging = $(this);
-        // $position = $($dragging).position();
-
-        // if ($position["top"] < 1 || $position["left"] < 1)  {
-        //   $($dragging).position["top"] = 10;
-        //   $dragging = null;
-        // };
-        // console.log($position);
-      });
-
+  });
   $(document.body).on("mouseup", function (e) {
     $dragging = null;
   });
+}
 
-    // Shuffle function
-    // This shuffles an array
-    // Make two variables for the original and the shuffled word
-    var myWord = null;
-    var shuffledWord = null;
+/////////////////// 2. Projects info /////////////////////////////////
 
-    function shuffle(a) {
-      var j, x, i;
-      for (i = a.length; i; i -= 1) {
-        j = Math.floor(Math.random() * i);
-        x = a[i - 1];
-        a[i - 1] = a[j];
-        a[j] = x;
-      }
-      shuffledWord = a.join("");
-    }
+function projectsInfo(){
+  // Projects: show info on hover and make other projects opaque
+   $(".project-refugapp").on("mouseover", function(){
+    $(".project-img").css("opacity", "0.6");
+    $(this).css("opacity", "1");
+    $(".footer-projects").text("Refugapp is a project we set up during the Le Wagon Bootcamp. We are creating a platform to bring locals and refugees together by exchanging skills. ");
+  })
 
-    function doShuffle(element){
-     var word = $(element).text();
-     var arrayWord = word.split("");
-     arrayWord.push(" ");
-     shuffle(arrayWord);
+   $(".project-sneakertours").on("mouseover", function(){
+    $(".project-img").css("opacity", "0.6");
+    $(this).css("opacity", "1");
+    $(".footer-projects").text("Sneakertours is a website I created with a friend to share the best places and events in Amsterdam.");
+  })
 
-     $(element).text(shuffledWord);
-   }
+   $(".project-plankgasten").on("mouseover", function(){
+    $(".project-img").css("opacity", "0.6");
+    $(this).css("opacity", "1");
+    $(".footer-projects").text("Plankgasten is a monthly open mic in CREA that I initiated and organised. After a short course I created this website.");
+  })
+}
 
-   var shuffleId1 = null;
-   var shuffleId2 = null;
+//////////////////// 3. Shuffle functions ////////////////////////////
+// shuffle the letters in the infobox
 
-   $(".info-list").mouseover( function(){
+function shuffleWords(){
+  shuffleAnimate();
+  function shuffle(arrayWord) {
+// Shuffle an array with the letters of the word
+// and make it a string again
+  var j, x, i;
+  for (i = arrayWord.length; i; i -= 1) {
+    j = Math.floor(Math.random() * i);
+    x = arrayWord[i - 1];
+    arrayWord[i - 1] = arrayWord[j];
+    arrayWord[j] = x;
+  }
+  shuffledWord = arrayWord.join("");
+}
+
+// Get the word out of the element, make an array of it and add spaces
+// Shuffle this array with the shufflefunction and set this as text
+function doShuffle(element){
+ var word = $(element).text();
+ var arrayWord = word.split("");
+ arrayWord.push(" ");
+ shuffle(arrayWord);
+
+ $(element).text(shuffledWord);
+}
+
+function shuffleAnimate(){
+  var shuffleId1 = null;
+  var shuffleId2 = null;
+
+  $(".info-list").mouseover( function(){
     // myWord = $(this).text();
+    // On hover set this as an element
     var element = this;
-
+    // Do the Shufflefunction 3 times with a small delay
     doShuffle(element);
-
     shuffleId1 = setTimeout(function(){
       doShuffle(element);
     }, 200);
-
     shuffleId2 = setTimeout(function(){
       doShuffle(element);
     }, 400);
-
+    // Set it back to the original text
     setTimeout(function(){
       var original = $(element).data('original');
       $(element).text(original);
     }, 650);
   })
 
-   $(".box-info").mouseout( function(){
-    //cancel all shuffle timeout
+  $(".box-info").mouseout( function(){
+    //cancel all shuffle timeout and set back the original text
     clearTimeout(shuffleId1);
     clearTimeout(shuffleId2);
     var items = $(".info-list");
@@ -137,34 +145,31 @@ $(document).ready(function(){
       $(this).text(original);
     });
   })
+}
+}
 
+// Let's enable all the functions when the doc is ready
+$(document).ready(function(){
 
-   // Projects: show info on hover
+ var myWord = null;
+ var shuffledWord = null;
 
-   $(".project-refugapp").on("mouseover", function(){
-    $(this).css("border", "10px 10px 5px 0px rgba(125,0,125,0.75)");
-    $(".footer-projects").text("Refugapp");
-  })
-
-   $(".project-refugapp").on("mouseout", function(){
-    $(this).css("opacity", "1");
-  })
-
-   $(".project-sneakertours").on("mouseover", function(){
-    $(".footer-projects").text("Sneakertours");
-  })
-
-   $(".project-plankgasten").on("mouseover", function(){
-    $(".footer-projects").text("Plankgasten");
-  })
-
-
+ closeBoxes();
+ infoBox();
+ activeBox();
+ boxDraggable();
+ projectsInfo();
+ shuffleWords();
 
  });
 
 
+ //////// function to write:
+ // When closing a box make the next one active
+ // Way to do it: remove closed box from array and
+ // push every newly open box to the array
+ // unless it is already in (in case of 2 clicks)
+ //////// Other stuff to improve
+ // Make nicer crosses
+ // Closer look at skills
 
-// $(".cross-box").click(function(){
-//   console.log("klik");
-//   // this.addClass("hidden");
-// });
